611ab6834164f0c553927854dcb27be8eb9dbfe5
CarlosEReis <carlos.er7@gmail.com>
2025-01-17 11:56:38 -0300

    feat: impl routes for dashboard and transactions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardModule)},
+  { path: 'transactions', loadChildren: () => import('./transactions/transactions.module').then( m => m.TransactionsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
