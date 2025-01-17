b258abcf39b11b63ab121799f7bc2ed6ea9fc166
CarlosEReis <carlos.er7@gmail.com>
2025-01-17 11:09:03 -0300

    feat: create navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'er7_finance-ui';
+
+  user: any;
+  menuItens?: MenuItem[];
+  menuProfile?: MenuItem[];
+  activeItem: MenuItem = [];
+
+  constructor(
+    private router: Router) {}
+
+  ngOnInit(): void {
+    this.menuItens = this.getMenuItens();
+    this.activeItem = this.menuItens[0];
+    this.menuProfile = this.getMenuProfile();
+  }
+
+  getMenuItens() {
+    return [
+      { label: 'Dashboard', icon: 'pi pi-chart-bar', command: () => this.router.navigate(['./dashboard']) },
+      { label: 'Transações', icon: 'pi pi-list', command: () => this.router.navigate(['./transactions']) },
+      { label: 'Assinatura', icon: 'pi pi-credit-card', command: () => this.router.navigate(['./subscription']) },
+    ];
+  }
+
+  logout() {
+    // Todo: implementar logout
+    alert('Logout')
+  }
+  
+  onActiveItemChange(event: MenuItem) {
+    this.activeItem = event;
+  }
+
+  getMenuProfile() {
+    return [
+      {
+        label: 'Profile',
+        items: [
+          {
+            separator: true,
+          },
+          {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            command: this.logout.bind(this),
+          },
+        ],
+      },
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import localePt from '@angular/common/locales/pt';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -6,8 +7,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { registerLocaleData } from '@angular/common';
 import { ToastModule } from 'primeng/toast';
-
-import localePt from '@angular/common/locales/pt';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenuModule } from 'primeng/menu'
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
 
 registerLocaleData(localePt);
 @NgModule({
@@ -19,7 +23,12 @@ registerLocaleData(localePt);
     BrowserAnimationsModule,
     AppRoutingModule,
 
-    ToastModule
+    ToastModule,
+    ToolbarModule,
+    TabMenuModule,
+    MenuModule,
+    ButtonModule,
+    AvatarModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt-BR' }
