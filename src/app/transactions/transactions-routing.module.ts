8828a715aebe7ab8fb50a1ced2a4bc640837ba2e
CarlosEReis <carlos.er7@gmail.com>
2025-01-23 16:38:13 -0300

    feat: config routes

--- src/app/transactions/transactions-routing.module.ts
+++ src/app/transactions/transactions-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TransactionsListComponent } from './transactions-list/transactions-list.component';
 import { AuthGuard, AuthService } from '@auth0/auth0-angular';
+import { TransactionFormComponent } from './transaction-form/transaction-form.component';
 
 const routes: Routes = [
-  { path: '', component: TransactionsListComponent, canActivate: [ AuthGuard ] }
+  { path: '', component: TransactionsListComponent, canActivate: [ AuthGuard ], children: [
+    { path: 'new', component: TransactionFormComponent, canActivate: [ AuthGuard ] },
+    { path: 'edit/:id', component: TransactionFormComponent, canActivate: [ AuthGuard ] }
+  ] },
 ];
 
 @NgModule({
