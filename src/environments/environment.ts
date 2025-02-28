ad9a232d488330ef52da002c2705944a93935ebf
CarlosEReis <carlos.er7@gmail.com>
2025-02-28 14:57:54 -0300

    env: api url prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    apiUrl: 'https://er7-financeai-api.onrender.com/',
+    auth0: {
+        domain: 'er7dev.us.auth0.com',
+        clientId: 'AVqmAwL7ZfcZNenxkL9NiZuyLAtf6mDR',
+    }  
+};
