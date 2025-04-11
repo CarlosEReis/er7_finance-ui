49bbe7dd82e660dfc4d1406b31da052191a2e9dd
CarlosEReis <carlos.er7@gmail.com>
2025-04-11 15:50:49 -0300

    chore: update API URL in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://teste5.er7.site/',
+    apiUrl: 'https://finances.er7.site/',
     auth0: {
         domain: 'er7dev.us.auth0.com',
         clientId: 'AVqmAwL7ZfcZNenxkL9NiZuyLAtf6mDR',
