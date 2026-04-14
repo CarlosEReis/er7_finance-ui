11b23958b55191780d7df9ba60f25b11aa97bc9a
CarlosEReis <carlos.er7@gmail.com>
2026-04-13 21:52:02 -0300

    fix: update environment API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://finances.er7.site/',
+    apiUrl: 'https://finance.er7.online/',
     auth0: {
         domain: 'er7dev.us.auth0.com',
         clientId: 'AVqmAwL7ZfcZNenxkL9NiZuyLAtf6mDR',
-    }  
+    }
 };
